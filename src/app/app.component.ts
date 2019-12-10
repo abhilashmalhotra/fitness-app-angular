252b76b65a0f5db7a0de7efc02460f31353aec8f
Abhilash <abhilashmalhotra17@gmail.com>
2019-12-10 22:47:33 +0530

    side nav & header seprated into different component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fitness-app';
+  @ViewChild('sideNav', {static : false}) sideNav;
+
+  // Toggle Navigation
+  toggleNavigation() {
+    this.sideNav.toggle()
+  }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule],
-    exports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule]
+    imports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule],
+    exports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule]
 })
 
 export class MaterialModule {
